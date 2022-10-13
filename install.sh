70d5bee45488a1468b544689890c9f5041ee0035
Den <weblifeon@gmail.com>
2022-10-13 12:55:07 +0300

    shell

---
+++ install.sh
@@ -0,0 +1,18 @@
+FILE=/etc/systemd/system/cicd.service
+
+cat <<EOF >$FILE
+[Unit]
+Description=CICD
+After=network.target
+
+[Service]
+Type=simple
+WorkingDirectory=${PWD}
+ExecStart=${PWD}/cicd
+Restart=on-failure
+RestartSec=15s
+StandardOutput=file:/${PWD}/main.log
+
+[Install]
+WantedBy=multi-user.target
+EOF
