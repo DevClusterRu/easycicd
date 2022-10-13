8262eeb52b791838070eb1f0173ada2470555792
Den <weblifeon@gmail.com>
2022-10-13 14:07:05 +0300

    ready

--- install.sh
+++ install.sh
@@ -11,8 +11,12 @@ WorkingDirectory=${PWD}
 ExecStart=${PWD}/cicd
 Restart=on-failure
 RestartSec=15s
-StandardOutput=file:/${PWD}/main.log
+StandardOutput=file:${PWD}/main.log
 
 [Install]
 WantedBy=multi-user.target
 EOF
+
+systemctl daemon-reload
+systemctl enable cicd.service
+systemctl start cicd.service
